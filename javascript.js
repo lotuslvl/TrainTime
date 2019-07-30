907f9f482cc844f2193ccdaa49583aaf11ab3ab3
LotusGit <lotuslindez@gmail.com>
2019-07-30 18:03:15 -0500

    base code, files and added firebase api

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB5CEgRqqT_FdEatfYNjgusblJaKoKrM8g",
+    authDomain: "fir-traintime-d5e6f.firebaseapp.com",
+    databaseURL: "https://fir-traintime-d5e6f.firebaseio.com",
+    projectId: "fir-traintime-d5e6f",
+    storageBucket: "fir-traintime-d5e6f.appspot.com",
+    messagingSenderId: "828994904923",
+    appId: "1:828994904923:web:ab5cc7cce92c8ee5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
